116852f71d7c10878d6756ffa779a33840831f0c
Howard Teh <77679600+HowardTeh1009@users.noreply.github.com>
2021-10-13 03:38:55 +0800

    Add files via upload

---
+++ L1_BasicSyntax/Program.cs
@@ -0,0 +1,21 @@
+﻿using System; // is like the program package such as include <stdio> in C Programming
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1_BasicSyntax // is the namespace that used to show the project title
+{
+    class Program // is a container for the data and methods - write all the codes inside this block
+    {
+        static void Main(string[] args) // this is the main method - only codes inside the main method will be executed
+        {
+            Console.WriteLine("Welcome to C# tutorial class."); // to display the output on the window console
+            Console.ReadLine(); // to let user press [ENTER] to end the program / press CTRL + F5 to see the output
+
+            /* notes: using c# in .NET framework will not show the output directly through inital debugging.
+             *        Either put Console.ReadLine() at the end of the program or use CTRL + F% to debug
+             */       
+        }
+    }
+}
