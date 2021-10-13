01a5085a39aac488c7925e0f835880ef7563b016
Howard Teh <77679600+HowardTeh1009@users.noreply.github.com>
2021-10-13 13:53:02 +0800

    Add files via upload

---
+++ L11_[OOP]_Constructors/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L11__OOP__Constructors
+{
+    class Program
+    {
+        /*
+         * L11_Constructors:
+            Constructor is s special method that is called when there is an object created.
+            It is used to initialise the value of the object variable which is also known
+            as the attribute of the class.
+
+            We may create the constructor by ourself or else Java will create the constructor
+            automatically. By default, the constructor that created automatically is not able to
+            initialise the value of the object attributes.
+
+            Why do we need constructor?
+                - constructor is needed when there is multiple objects and we do not want
+                the all objects to have the same values.
+        */
+        static void Main(string[] args)
+        {
+            // create a class object by using the constructor
+            Car car = new Car("PMP1234", "Red", "Mustang", 2008); // using constructor can save a lot of time in creating an object compared to the method that we using in the last lecture
+
+            Console.WriteLine("The color of the car is " + car.color);
+            Console.WriteLine("The plate number of the car is " + car.plateNo);
+            Console.WriteLine("The brand of the car is " + car.brand);
+            Console.WriteLine("The car is manufactured on " + car.mfcYear);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Car // create a new class called Car
+    {
+        // class attributes
+        public string plateNo, color, brand;
+        public int mfcYear;
+
+        // create the class constructor method
+        public Car(string plateNo, string color, string brand, int year) // the constructor must be public and have the same name as the class name
+        {
+            this.plateNo = plateNo; // this keyword is use to emphasize the current class attributes is equal to the parameter value if both class attribute and constructor parameter are having the same name
+            this.color = color;
+            this.brand = brand;
+            mfcYear = year; // here we do not use this keyword because I want to show the difference of not using the this keyword while we have the difference attribute name
+        }
+
+        // class methods
+        public void accerelate(int speed)
+        {
+            Console.WriteLine($"The car is accerelate at the speed of {speed} km/h.");
+        }
+
+        public void deccerelate(int speed)
+        {
+            Console.WriteLine($"The car is deccerelate and the speed now is {speed} km/h.");
+        }
+    }
+}
---
+++ L12_[OOP]_Encapsulation/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L12__OOP__Encapsulation
+{
+    class Program
+    {
+        /*  L12_Encapsulation:
+            Encapsulation in Java means that the attributes of a class will be hidden or private.
+            The private attributes can only be accessed only through properties (getters & setters).
+            You should make the attributes private if you want to keep those attributes confidential.
+
+            Why we should use encapsulation?
+                - Better control of class attributes and methods
+                - Class attributes can be made read-only (if you only use the get method), or write-only (if you only use the set method)
+                - Flexible: the programmer can change one part of the code without affecting other parts
+                - Increased security of data
+        */
+        static void Main(string[] args)
+        {
+            // create the car object
+            Car car = new Car("PMP1234", "Red", "Mustang", 2018);
+
+            // modify or set the private attribute values using properties
+            car.PlateNo = "PKM7369";
+
+            // retrieve the value of the new private attribute values using properties
+            Console.WriteLine($"The new plate number is {car.PlateNo}");
+
+            Console.ReadLine();
+        }
+    }
+
+    class Car // create a new class called Car
+    {
+        // create class attributes that are private using private keywords
+        private string plateNo, color, brand;
+        private int mfcYear;
+
+        // class constructors
+        public Car(string plateNo, string color, string brand, int mfcYear)
+        {
+            this.plateNo = plateNo;
+            this.color = color;
+            this.brand = brand;
+            this.mfcYear = mfcYear;
+        }
+
+        // create class properties (getters and setters in C#)
+        // 1st way
+        public string PlateNo
+        {
+            get { return plateNo; }
+            set { plateNo = value; }
+        }
+
+        // 2nd way - shorhand/shorcut => this way is easier and less code
+        public string Color { get; set;}
+        public string Brand { get; set; }
+        public int MfcYear { get; set; }
+
+        // class methods
+        public void accerelate(int speed)
+        {
+            Console.WriteLine($"The car is accerelate at the speed of {speed} km/h.");
+        }
+
+        public void deccerelate(int speed)
+        {
+            Console.WriteLine($"The car is deccerelate and the speed now is {speed} km/h.");
+        }
+    }
+}
